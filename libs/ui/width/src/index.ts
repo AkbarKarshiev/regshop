8901b4179a696f92dae9786047cd27c3e36f0da6
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-04-19 12:24:27 +0500

    Addition: full-width directive

---
+++ libs/ui/width/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/width.module';
---
+++ libs/ui/width/src/lib/full-width.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[regshopFullWidth], [regshop-full-width]'
+})
+export class FullWidthDirective {
+
+}
