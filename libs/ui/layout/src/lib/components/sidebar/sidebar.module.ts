ea63d27ffe9016f79bfd817207ccc5d75456b80b
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-04-29 11:31:02 +0500

    Addition: sidebar complete

--- libs/ui/layout/src/lib/components/sidebar/sidebar.module.ts
+++ libs/ui/layout/src/lib/components/sidebar/sidebar.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { SidebarComponent } from './sidebar.component';
+import { LogoModule } from '../logo/logo.module';
+import { NavModule } from '../nav/nav.module';
+import { CopyrightModule } from '../footer/components/copyright/copyright.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SidebarComponent],
+  imports: [CommonModule, LogoModule, NavModule, CopyrightModule],
+  exports: [SidebarComponent]
 })
 export class SidebarModule { }
