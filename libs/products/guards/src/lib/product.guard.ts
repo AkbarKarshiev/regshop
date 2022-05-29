570cd4607e9d528f3b1e172a97c640ea94bebf5c
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-05-29 11:10:29 +0500

    Addition: Product guards

---
+++ libs/products/guards/src/lib/product.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, UrlTree } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { NavigationService } from '@regshop/core/navigation/service';
+import { ProductFacade } from '@regshop/products/state';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductGuard implements CanActivate {
+  private readonly redirectTree = this.navigationService.createUrlTree(this.navigationService.getPaths().home);
+
+  constructor(private readonly navigationService: NavigationService, private readonly productFacade: ProductFacade) {}
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> {
+    const { slug } = route.params;
+
+    if (!slug) {
+      return of(this.redirectTree);
+    }
+
+    return this.productFacade.productBySlugLoaded$(slug).pipe(map((product) => !!product || this.redirectTree));
+  }
+}
